559246471a7fa5bc0de923bf53fa2075aede8fc3
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-09-27 22:17:23 +0300

    Some codewars

---
+++ js/codewars.js
@@ -0,0 +1,41 @@
+function positiveSum(arr) {
+    total = 0
+    
+    for (let i=0; i<arr.length; i++) {
+      if(arr[i] > 0){
+        total += arr[i];
+      }
+    }
+    return total
+  }
+  console.log((positiveSum([1,2,-3,4,5])));
+
+  function arrayMadness(a, b) {
+      sum1 = 0;
+      sum2 = 0;
+    
+    if (a.length || b.length >= 1) {
+        
+    for (let i = 0; i < a.length; i++) {
+        sum1 += a[i]**2;
+    }
+    for (let i = 0; i < b.length; i++) {
+        sum2 += b[i]**3;
+    }
+    }
+    if (sum1 > sum2) {
+        return true
+    }
+    return false
+  }
+console.log(arrayMadness([4,5,6],[1,2,3]), true);
+console.log(arrayMadness([5,6,7],[4,5,6]), false);
+console.log(arrayMadness([4,5,6],[3,4,5]), false);
+console.log(arrayMadness([3,4,5],[2,3,4]), false);
+console.log(arrayMadness([2,3,4],[1,2,3]), false);
+console.log(arrayMadness([1,2,3],[0,1,2]), true);
+console.log(arrayMadness([5,3,2,4,1],[15]), false);
+console.log(arrayMadness([2,5,3,4,1],[3,3,3,3,3]), false);
+console.log(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]), false);
+console.log(arrayMadness([1,2,3,4,5],[2,2,2,2,2,2,1,1,1]), true);
+console.log(arrayMadness([2,4,6,8,10,12,14],[1,3,5,7,9,11,13]), false);
